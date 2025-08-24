3b6453d7151e67f18a7b58f87bbcc2a51965771d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-08-24 15:58:44 -0500

    omg

--- main.rs
+++ main.rs
@@ -1,8 +1,25 @@
 use futures_lite::stream;
+use nusb::hotplug::HotplugEvent;
 
 fn main() {
     env_logger::init();
     for event in stream::block_on(nusb::watch_devices().unwrap()) {
-        println!("{:#?}", event);
+        // TODO: match events over HotplugEvent::Connected and HotplugEvent::Disconnected
+        match event {
+            HotplugEvent::Connected(device_info) => {
+                print_device_info(&device_info);
+            }
+            HotplugEvent::Disconnected(device_id) => {
+                print_device_id(&device_id);
+            }
+        }
     }
+}
+
+fn print_device_id(device_id: &nusb::DeviceId) {
+    println!("dis: {:?}", device_id)
+}
+
+fn print_device_info(d: &nusb::DeviceInfo) {
+    println!("c: {:?}, {:?}, ({:?})", d.manufacturer_string(), d.product_string(), d.id());
 }
