3fae201bd44e6f17ebb7449f22799fa2ce0f5ffe
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-08-24 16:27:46 -0500

    Ok this works

--- main.rs
+++ main.rs
@@ -5,42 +5,41 @@ use std::collections::HashMap;
 pub const LOGI_USB_DEVICES: &str = "046d:c547";
 
 fn main() -> anyhow::Result<()> {
-    env_logger::init();
+  env_logger::init();
 
-    futures_lite::future::block_on(async {
-        let mut events = nusb::watch_devices()?;
-        let mut devices: HashMap<nusb::DeviceId, (u16, u16)> = HashMap::new();
+  futures_lite::future::block_on(async {
+    let mut events = nusb::watch_devices()?;
+    let mut devices: HashMap<nusb::DeviceId, (u16, u16)> = HashMap::new();
 
-        while let Some(event) = events.next().await {
-            match event {
-                HotplugEvent::Connected(info) => {
-                    let id = info.id();
-                    let vendor = info.vendor_id();
-                    let product = info.product_id();
+    while let Some(event) = events.next().await {
+      match event {
+        HotplugEvent::Connected(info) => {
+          let id = info.id();
+          let vendor = info.vendor_id();
+          let product = info.product_id();
+          let device_str = format!("{:04x}:{:04x}", vendor, product);
 
-                    println!(
-                        "Connected: {:04x}:{:04x}, {:?}",
-                        vendor, product, info.manufacturer_string()
-                    );
+          if device_str == LOGI_USB_DEVICES {
+            println!("Found Logitech USB device: {}", device_str);
+          }
 
-                    // Cache vendor/product by DeviceId
-                    devices.insert(id, (vendor, product));
-                }
-                HotplugEvent::Disconnected(id) => {
-                    if let Some((vendor, product)) = devices.remove(&id) {
-                        println!(
-                            "Disconnected: {:04x}:{:04x} (id={:?})",
-                            vendor, product, id
-                        );
-                    } else {
-                        println!("Disconnected unknown device: {:?}", id);
-                    }
-                }
-            }
+          // Cache vendor/product by DeviceId
+          devices.insert(id, (vendor, product));
         }
+        HotplugEvent::Disconnected(id) => {
+          if let Some((vendor, product)) = devices.remove(&id) {
+            let device_str = format!("{:04x}:{:04x}", vendor, product);
 
-        Ok::<_, anyhow::Error>(())
-    })?;
+            if device_str == LOGI_USB_DEVICES {
+              println!("Disconnected Logitech USB device: {}", device_str);
+            }
+          }
+        }
+      }
+    }
 
-    Ok(())
+    Ok::<_, anyhow::Error>(())
+  })?;
+
+  Ok(())
 }
