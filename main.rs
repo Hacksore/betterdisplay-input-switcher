749ef2fa2f6108ba2c06312266fffda15b82d107
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-08-24 16:13:05 -0500

    Current impl

--- main.rs
+++ main.rs
@@ -1,25 +1,25 @@
 use futures_lite::stream;
 use nusb::hotplug::HotplugEvent;
 
+pub const LOGI_USB_ID: &str = "4294994929";
+
+// Device connected: 1133:50503 (Some("USB Receiver") - Some
+
 fn main() {
     env_logger::init();
     for event in stream::block_on(nusb::watch_devices().unwrap()) {
         // TODO: match events over HotplugEvent::Connected and HotplugEvent::Disconnected
         match event {
-            HotplugEvent::Connected(device_info) => {
-                print_device_info(&device_info);
+            HotplugEvent::Connected(d) => {
+                let id = format!("{:?}", d.id());
+                let product = format!("{:?} - {:?}", d.product_string(), d.manufacturer_string());
+                println!("Device Connected: {} ({})", id, product);
             }
-            HotplugEvent::Disconnected(device_id) => {
-                print_device_id(&device_id);
+            HotplugEvent::Disconnected(d) => {
+                let id = format!("{:?}", d);
+                println!("Device Disconnected: {}", id);
             }
         }
     }
 }
 
-fn print_device_id(device_id: &nusb::DeviceId) {
-    println!("dis: {:?}", device_id)
-}
-
-fn print_device_info(d: &nusb::DeviceInfo) {
-    println!("c: {:?}, {:?}, ({:?})", d.manufacturer_string(), d.product_string(), d.id());
-}
